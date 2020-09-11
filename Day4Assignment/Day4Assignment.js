56976e1a026d719a4a0bc18a8105641f3d57e06c
Darshan <darshanbangera1612@gmail.com>
2020-09-11 16:58:51 +0530

    Create Day4Assignment.js

---
+++ Day4Assignment/Day4Assignment.js
@@ -0,0 +1,61 @@
+// // Question1
+// function changeImage0(){
+//     const ele0=document.getElementById("Image");
+//     const newUrl0="https://images.firstpost.com/wp-content/uploads/2019/04/thor380.jpg"
+
+//     ele0.src=newUrl0;
+
+// }
+
+// function changeImage1(){
+//     const ele1=document.getElementById("Image");
+//     const newUrl1="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRwe7c_m_hpIY4wMcF3LGG31zod0Dshe9odgA&usqp=CAU"
+
+//     ele1.src=newUrl1;
+
+// }
+// function changeImage2(){
+//     const ele2=document.getElementById("Image");
+//     const newUrl2="https://media.wired.com/photos/5932bdcd95879f6d0c009d66/master/pass/thor-hemsworth.jpg"
+
+//     ele2.src=newUrl2;
+// }
+
+// Question3
+// let BioData3={
+//     name:"Letsupgrade",
+//     age:"30",
+//     country:"India",
+//     hobbies:["Teaching","Learning"],
+//     printData:function(){
+//         console.log(this);
+//     },
+// };
+// BioData3.printData();
+
+// Question4
+// let BioData4={
+//     name:["Letsupgrade","Udemy","Byjus","UpGrad"],
+//     age:[30,20,25,18],
+//     country:["India","USA","India","India"],
+// };
+// function displayAge(){
+//     BioData4.age.forEach(function(ele) {
+//         if(ele<30){
+//             console.log(ele);
+//         }
+        
+
+//     });
+// }
+// function displayCountry(){
+//     BioData4.country.forEach(function(ele) {
+//         if(ele=="India"){
+//             console.log(ele);
+//         }
+        
+
+//     });
+// }
+// displayCountry();
+// displayAge();
